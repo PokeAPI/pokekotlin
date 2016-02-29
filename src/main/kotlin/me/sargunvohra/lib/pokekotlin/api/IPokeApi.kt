b90b065aeae4b7d947dd4edffc2a8e461429d783
Sargun Vohra <sargunster@gmail.com>
2016-02-29 13:46:11 -0500

    all remaining endpoints

--- src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/IPokeApi.kt
+++ src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/IPokeApi.kt
@@ -230,4 +230,146 @@ internal interface IPokeApi {
     fun getEncounterConditionValue(@Path("id") id: Int): Call<EncounterConditionValue>
 
     // endregion Contests
+
+    // region Evolution
+
+    @GET("evolution-chain/{id}/")
+    fun getEvolutionChain(@Path("id") id: Int): Call<EvolutionChain>
+
+    @GET("evolution-trigger/{id}/")
+    fun getEvolutionTrigger(@Path("id") id: Int): Call<EvolutionTrigger>
+
+    // endregion Evolution
+
+    // region Games
+
+    @GET("generation/{id}/")
+    fun getGeneration(@Path("id") id: Int): Call<Generation>
+
+    @GET("pokedex/{id}/")
+    fun getPokedex(@Path("id") id: Int): Call<Pokedex>
+
+    @GET("version/{id}/")
+    fun getVersion(@Path("id") id: Int): Call<Version>
+
+    @GET("version-group/{id}/")
+    fun getVersionGroup(@Path("id") id: Int): Call<VersionGroup>
+
+    // endregion Games
+
+    // region Items
+
+    @GET("item/{id}/")
+    fun getItem(@Path("id") id: Int): Call<Item>
+
+    @GET("item-attribute/{id}/")
+    fun getItemAttribute(@Path("id") id: Int): Call<ItemAttribute>
+
+    @GET("item-category/{id}/")
+    fun getItemCategory(@Path("id") id: Int): Call<ItemCategory>
+
+    @GET("item-fling-effect/{id}/")
+    fun getItemFlingEffect(@Path("id") id: Int): Call<ItemFlingEffect>
+
+    @GET("item-pocket/{id}/")
+    fun getItemPocket(@Path("id") id: Int): Call<ItemPocket>
+
+    // endregion Items
+
+    // region Moves
+
+    @GET("move/{id}/")
+    fun getMove(@Path("id") id: Int): Call<Move>
+
+    @GET("move-ailment/{id}/")
+    fun getMoveAilment(@Path("id") id: Int): Call<MoveAilment>
+
+    @GET("move-battle-style/{id}/")
+    fun getMoveBattleStyle(@Path("id") id: Int): Call<MoveBattleStyle>
+
+    @GET("move-category/{id}/")
+    fun getMoveCategory(@Path("id") id: Int): Call<MoveCategory>
+
+    @GET("move-damage-class/{id}/")
+    fun getMoveDamageClass(@Path("id") id: Int): Call<MoveDamageClass>
+
+    @GET("move-learn-method/{id}/")
+    fun getMoveLearnMethod(@Path("id") id: Int): Call<MoveLearnMethod>
+
+    @GET("move-target/{id}/")
+    fun getMoveTarget(@Path("id") id: Int): Call<MoveTarget>
+
+    // endregion Moves
+
+    // region Locations
+
+    @GET("location/{id}/")
+    fun getLocation(@Path("id") id: Int): Call<Location>
+
+    @GET("location-area/{id}/")
+    fun getLocationArea(@Path("id") id: Int): Call<LocationArea>
+
+    @GET("pal-park-area/{id}/")
+    fun getPalParkArea(@Path("id") id: Int): Call<PalParkArea>
+
+    @GET("region/{id}/")
+    fun getRegion(@Path("id") id: Int): Call<Region>
+
+    // endregion Locations
+
+    // region Pokemon
+
+    @GET("ability/{id}/")
+    fun getAbility(@Path("id") id: Int): Call<Ability>
+
+    @GET("characteristic/{id}/")
+    fun getCharacteristic(@Path("id") id: Int): Call<Characteristic>
+
+    @GET("egg-group/{id}/")
+    fun getEggGroup(@Path("id") id: Int): Call<EggGroup>
+
+    @GET("gender/{id}/")
+    fun getGender(@Path("id") id: Int): Call<Gender>
+
+    @GET("growth-rate/{id}/")
+    fun getGrowthRate(@Path("id") id: Int): Call<GrowthRate>
+
+    @GET("nature/{id}/")
+    fun getNature(@Path("id") id: Int): Call<Nature>
+
+    @GET("pokeathlon-stat/{id}/")
+    fun getPokeathlonStat(@Path("id") id: Int): Call<PokeathlonStat>
+
+    @GET("pokemon/{id}/")
+    fun getPokemon(@Path("id") id: Int): Call<Pokemon>
+
+    @GET("pokemon-color/{id}/")
+    fun getPokemonColor(@Path("id") id: Int): Call<PokemonColor>
+
+    @GET("pokemon-form/{id}/")
+    fun getPokemonForm(@Path("id") id: Int): Call<PokemonForm>
+
+    @GET("pokemon-habitat/{id}/")
+    fun getPokemonHabitat(@Path("id") id: Int): Call<PokemonHabitat>
+
+    @GET("pokemon-shape/{id}/")
+    fun getPokemonShape(@Path("id") id: Int): Call<PokemonShape>
+
+    @GET("pokemon-species/{id}/")
+    fun getPokemonSpecies(@Path("id") id: Int): Call<PokemonSpecies>
+
+    @GET("stat/{id}/")
+    fun getStat(@Path("id") id: Int): Call<Stat>
+
+    @GET("type/{id}/")
+    fun getType(@Path("id") id: Int): Call<Type>
+
+    // endregion Pokemon
+
+    // region Utility
+
+    @GET("language/{id}/")
+    fun getLanguage(@Path("id") id: Int): Call<Language>
+
+    // endregion Utility
 }
