26f75bfe6a8d3b257c8175545a034ca9b45103c9
Sargun Vohra <sargunster@gmail.com>
2016-03-06 01:13:23 -0500

    0.3.0

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -17,7 +17,7 @@ val p = project {
     name = "PokeKotlin"
     group = "me.sargunvohra.lib"
     artifactId = name
-    version = "0.2.0"
+    version = "0.3.0"
 
     sourceDirectories {
         path("src/main/kotlin")
