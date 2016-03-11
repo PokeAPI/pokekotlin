ca01c8be42e7c99242c32cee50d3606110c380db
Sargun Vohra <sargunster@gmail.com>
2016-03-11 01:43:21 -0500

    increase timeout

--- src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApi.kt
+++ src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApi.kt
@@ -1,11 +1,20 @@
 package me.sargunvohra.lib.pokekotlin.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object PokeApi : IPokeApi by Retrofit.Builder()
         .baseUrl(PokeApi.rootUrl)
         .addConverterFactory(MoshiConverterFactory.create())
+        .client(
+                OkHttpClient.Builder()
+                        .connectTimeout(20, TimeUnit.SECONDS)
+                        .readTimeout(20, TimeUnit.SECONDS)
+                        .writeTimeout(20, TimeUnit.SECONDS)
+                        .build()
+        )
         .build()
         .create(IPokeApi::class.java) {
     val rootUrl = "http://pokeapi.co/api/v2/"
