40d8770e2a829ac6090c14eb2eb74b2a0dc46c90
Sargun Vohra <sargunster@gmail.com>
2016-03-12 22:23:02 -0500

    customizable PokeApiClient

--- src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApi.kt
+++ src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApi.kt
@@ -1,21 +1,3 @@
 package me.sargunvohra.lib.pokekotlin.api
 
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
-
-object PokeApi : IPokeApi by Retrofit.Builder()
-        .baseUrl(PokeApi.rootUrl)
-        .addConverterFactory(MoshiConverterFactory.create())
-        .client(
-                OkHttpClient.Builder()
-                        .connectTimeout(20, TimeUnit.SECONDS)
-                        .readTimeout(20, TimeUnit.SECONDS)
-                        .writeTimeout(20, TimeUnit.SECONDS)
-                        .build()
-        )
-        .build()
-        .create(IPokeApi::class.java) {
-    val rootUrl = "http://pokeapi.co/api/v2/"
-}
+object PokeApi : IPokeApi by PokeApiClient()
---
+++ src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApiClient.kt
@@ -0,0 +1,21 @@
+package me.sargunvohra.lib.pokekotlin.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+class PokeApiClient(
+        val rootUrl: String = "http://pokeapi.co/api/v2/",
+        val httpClient: OkHttpClient = OkHttpClient.Builder()
+                .retryOnConnectionFailure(false)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .build()
+) : IPokeApi by Retrofit.Builder()
+        .baseUrl(rootUrl)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .client(httpClient)
+        .build()
+        .create(IPokeApi::class.java)
