0a9dc6714a576ae3ed88e7f5020d5c17af1a406c
Sargun Vohra <sargunster@gmail.com>
2016-05-13 15:29:20 -0400

    test utility, close #7

---
+++ src/test/kotlin/me/sargunvohra/lib/pokekotlin/test/UtilityTest.kt
@@ -0,0 +1,32 @@
+package me.sargunvohra.lib.pokekotlin.test
+
+import me.sargunvohra.lib.pokekotlin.api.PokeApi
+import me.sargunvohra.lib.pokekotlin.api.promise
+import me.sargunvohra.lib.pokekotlin.json.Name
+import me.sargunvohra.lib.pokekotlin.json.NamedApiResource
+import org.testng.annotations.Test
+import kotlin.test.*
+
+class UtilityTest {
+
+    @Test
+    fun getLanguage() {
+        PokeApi.getLanguage(9).promise.get().apply {
+            assertEquals(9, id)
+            assertEquals("us", iso3166)
+            assertEquals("en", iso639)
+            assertEquals("en", name)
+            assert(
+                    names.contains(
+                            Name(
+                                    name = "English",
+                                    language = NamedApiResource(
+                                            name = "en",
+                                            url = PokeApi.rootUrl + "language/9/"
+                                    )
+                            )
+                    )
+            )
+        }
+    }
+}
