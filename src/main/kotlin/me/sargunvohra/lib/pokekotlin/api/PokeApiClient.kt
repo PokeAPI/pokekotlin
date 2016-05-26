952d1cb6252b20a23b7e2cc9f267cf1b28f877c1
Sargun Vohra <sargunster@gmail.com>
2016-05-25 21:46:19 -0400

    remove temp url

--- src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApiClient.kt
+++ src/main/kotlin/me/sargunvohra/lib/pokekotlin/api/PokeApiClient.kt
@@ -7,7 +7,6 @@ import java.util.concurrent.TimeUnit
 
 open class PokeApiClient(
         val rootUrl: String = "http://pokeapi.co/api/v2/",
-//        val rootUrl: String = "http://localhost:8000/api/v2/",
         val httpClient: OkHttpClient = OkHttpClient.Builder()
                 .retryOnConnectionFailure(false)
                 .connectTimeout(20, TimeUnit.SECONDS)
