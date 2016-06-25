a35a46130b69085374282bc13fcec607a05ef63e
Sargun Vohra <sargunster@gmail.com>
2016-06-25 19:34:37 -0400

    2.1.0: Interface for resource summary and list (#39)

--- src/main/kotlin/me/sargunvohra/lib/pokekotlin/model/resource.kt
+++ src/main/kotlin/me/sargunvohra/lib/pokekotlin/model/resource.kt
@@ -1,12 +1,38 @@
 package me.sargunvohra.lib.pokekotlin.model
 
+interface ResourceSummary {
+    val id: Int
+    val category: String
+}
+
 data class ApiResource(
-        val category: String,
-        val id: Int
-)
+        override val category: String,
+        override val id: Int
+) : ResourceSummary
 
 data class NamedApiResource(
         val name: String,
-        val category: String,
-        val id: Int
-)
+        override val category: String,
+        override val id: Int
+) : ResourceSummary
+
+interface ResourceSummaryList<T : ResourceSummary> {
+    val count: Int
+    val next: String?
+    val previous: String?
+    val results: List<T>
+}
+
+data class ApiResourceList(
+        override val count: Int,
+        override val next: String?,
+        override val previous: String?,
+        override val results: List<ApiResource>
+) : ResourceSummaryList<ApiResource>
+
+data class NamedApiResourceList(
+        override val count: Int,
+        override val next: String?,
+        override val previous: String?,
+        override val results: List<NamedApiResource>
+) : ResourceSummaryList<NamedApiResource>
