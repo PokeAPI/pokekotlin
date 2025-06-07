cb111e07a4bcdd08a6d1eed9b5c84913a71bd1a3
Sargun Vohra <sargunv@users.noreply.github.com>
2025-06-06 22:43:49 -0700

    handle exceptions

--- src/commonMain/kotlin/dev/sargunv/pokekotlin/PokeApiError.kt
+++
@@ -1,3 +0,0 @@
-package dev.sargunv.pokekotlin
-
-class PokeApiError(val code: Int, message: String) : Throwable("($code) $message")
--- src/commonTest/kotlin/dev/sargunv/pokekotlin/test/LiveTest.kt
+++ src/commonTest/kotlin/dev/sargunv/pokekotlin/test/LiveTest.kt
@@ -1,18 +1,27 @@
 package dev.sargunv.pokekotlin.test
 
 import dev.sargunv.pokekotlin.PokeApi
+import dev.sargunv.pokekotlin.PokeApiException
+import io.ktor.http.HttpStatusCode.Companion.NotFound
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlinx.coroutines.test.runTest
 
 @Ignore
 class LiveTest {
 
-  @Test fun liveObject() = runTest { assertEquals("sitrus", PokeApi.getBerry(10).name) }
+  @Test fun resource() = runTest { assertEquals("sitrus", PokeApi.getBerry(10).name) }
 
   @Test
-  fun liveList() = runTest {
+  fun list() = runTest {
     assertEquals(PokeApi.getMoveList(0, 50).results[25], PokeApi.getMoveList(25, 50).results[0])
+  }
+
+  @Test
+  fun notFound() = runTest {
+    val e = assertFailsWith(PokeApiException.HttpStatus::class) { PokeApi.getMove(-1) }
+    assertEquals(NotFound, e.status)
   }
 }
