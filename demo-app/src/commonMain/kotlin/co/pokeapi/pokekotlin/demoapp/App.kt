5da1c226662fe8abe386260b01868876362283cf
Sargun Vohra <sargunv@users.noreply.github.com>
2025-06-10 02:57:40 -0700

    all but wasm working

--- demo-app/src/commonMain/kotlin/co/pokeapi/pokekotlin/demoapp/App.kt
+++ demo-app/src/commonMain/kotlin/co/pokeapi/pokekotlin/demoapp/App.kt
@@ -1,15 +1,54 @@
 package co.pokeapi.pokekotlin.demoapp
 
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import co.pokeapi.pokekotlin.PokeApi
+import co.pokeapi.pokekotlin.model.NamedApiResource
 
 @Composable
-fun DemoApp(navController: NavHostController = rememberNavController()) {
-  MaterialTheme(colorScheme = getDefaultColorScheme()) { Text("Hello, PokeKotlin") }
+fun DemoApp() {
+  MaterialTheme(colorScheme = getDefaultColorScheme()) {
+    val species = remember { mutableStateListOf<NamedApiResource>() }
+    var isLoading by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+      val list = PokeApi.getPokemonSpeciesList(0, 10000)
+      species.clear()
+      species.addAll(list.results)
+      isLoading = false
+    }
+
+    Scaffold(
+      topBar = { TopAppBar(title = { Text("PokeKotlin Demo") }) },
+      content = { innerPadding ->
+        LazyColumn(
+          modifier = Modifier.consumeWindowInsets(innerPadding),
+          contentPadding = innerPadding,
+        ) {
+          if (isLoading) {
+            item { CircularProgressIndicator() }
+          } else {
+            items(species) { it -> Text(it.name) }
+          }
+        }
+      },
+    )
+  }
 }
 
 @Composable expect fun getDefaultColorScheme(isDark: Boolean = false): ColorScheme
