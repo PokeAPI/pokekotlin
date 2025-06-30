f43f281192b0c4050e15d12ce0f96347ffc0e269
Sargun Vohra <sargunv@users.noreply.github.com>
2025-06-30 01:03:56 -0400

    add kdoc for games and items

--- src/commonMain/kotlin/co/pokeapi/pokekotlin/model/games.kt
+++ src/commonMain/kotlin/co/pokeapi/pokekotlin/model/games.kt
@@ -3,6 +3,21 @@ package co.pokeapi.pokekotlin.model
 import co.pokeapi.pokekotlin.internal.JsOnlyExport
 import kotlinx.serialization.Serializable
 
+/**
+ * A generation is a grouping of the Pokémon games that separates them based on the Pokémon they
+ * include. In each generation, a new set of Pokémon, moves, abilities and types that did not exist
+ * in the previous generation are released. See: https://pokeapi.co/docs/v2#generations
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param abilities A list of abilities that were introduced in this generation.
+ * @param names The name of this resource listed in different languages.
+ * @param mainRegion The main region travelled in this generation.
+ * @param moves A list of moves that were introduced in this generation.
+ * @param pokemonSpecies A list of Pokémon species that were introduced in this generation.
+ * @param types A list of types that were introduced in this generation.
+ * @param versionGroups A list of version groups that were introduced in this generation.
+ */
 @Serializable
 @JsOnlyExport
 public data class Generation(
@@ -17,6 +32,21 @@ public data class Generation(
   val versionGroups: List<NamedApiResource>,
 )
 
+/**
+ * A Pokédex is a handheld electronic encyclopedia device; one which is capable of recording and
+ * retaining information of the various Pokémon in a given region with the exception of the national
+ * dex and some smaller dexes related to portions of a region. See:
+ * https://pokeapi.co/docs/v2#pokedexes
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param isMainSeries Whether or not this Pokédex originated in the main series of the video games.
+ * @param descriptions The description of this resource listed in different languages.
+ * @param names The name of this resource listed in different languages.
+ * @param pokemonEntries A list of Pokémon catalogued in this Pokédex and their indexes.
+ * @param region The region this Pokédex catalogues Pokémon for.
+ * @param versionGroups A list of version groups this Pokédex is relevant to.
+ */
 @Serializable
 @JsOnlyExport
 public data class Pokedex(
@@ -30,10 +60,24 @@ public data class Pokedex(
   val versionGroups: List<NamedApiResource>,
 )
 
+/**
+ * A Pokémon species entry within a Pokédex. See: https://pokeapi.co/docs/v2#pokemonentry
+ *
+ * @param entryNumber The index of this Pokémon species entry within the Pokédex.
+ * @param pokemonSpecies The Pokémon species being encountered.
+ */
 @Serializable
 @JsOnlyExport
 public data class PokemonEntry(val entryNumber: Int, val pokemonSpecies: NamedApiResource)
 
+/**
+ * Versions of the games, e.g Red, Blue or Yellow. See: https://pokeapi.co/docs/v2#versions
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param names The name of this resource listed in different languages.
+ * @param versionGroup The version group this version belongs to.
+ */
 @Serializable
 @JsOnlyExport
 public data class Version(
@@ -43,6 +87,20 @@ public data class Version(
   val versionGroup: NamedApiResource,
 )
 
+/**
+ * Version groups categorize highly similar versions of the games. See:
+ * https://pokeapi.co/docs/v2#version-groups
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param order Order for sorting. Almost by date of release, except similar versions are grouped
+ *   together.
+ * @param generation The generation this version was introduced in.
+ * @param moveLearnMethods A list of methods in which Pokémon can learn moves in this version group.
+ * @param pokedexes A list of Pokédexes introduces in this version group.
+ * @param regions A list of regions that can be visited in this version group.
+ * @param versions The versions this version group owns.
+ */
 @Serializable
 @JsOnlyExport
 public data class VersionGroup(
--- src/commonMain/kotlin/co/pokeapi/pokekotlin/model/items.kt
+++ src/commonMain/kotlin/co/pokeapi/pokekotlin/model/items.kt
@@ -3,6 +3,27 @@ package co.pokeapi.pokekotlin.model
 import co.pokeapi.pokekotlin.internal.JsOnlyExport
 import kotlinx.serialization.Serializable
 
+/**
+ * An item is an object in the games which the player can pick up, keep in their bag, and use in
+ * some manner. They have various uses, including healing, powering up, helping catch Pokémon, or to
+ * access a new area. See: https://pokeapi.co/docs/v2#items
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param cost The price of this item in stores.
+ * @param flingPower The power of the move Fling when used with this item.
+ * @param flingEffect The effect of the move Fling when used with this item.
+ * @param attributes A list of attributes this item has.
+ * @param category The category of items this item falls into.
+ * @param effectEntries The effect of this item listed in different languages.
+ * @param flavorTextEntries The flavor text of this item listed in different languages.
+ * @param gameIndices A list of game indices relevent to this item by generation.
+ * @param names The name of this item listed in different languages.
+ * @param heldByPokemon A list of Pokémon that might be found in the wild holding this item.
+ * @param babyTriggerFor An evolution chain this item requires to produce a baby during mating.
+ * @param sprites A set of sprites used to depict this item in the game.
+ * @param machines A list of the machines related to this item.
+ */
 @Serializable
 @JsOnlyExport
 public data class Item(
@@ -23,8 +44,20 @@ public data class Item(
   val machines: List<MachineVersionDetail>,
 )
 
+/**
+ * The sprites used to depict an item in the game. See: https://pokeapi.co/docs/v2#itemsprites
+ *
+ * @param default The default depiction of this item.
+ */
 @Serializable @JsOnlyExport public data class ItemSprites(val default: String?)
 
+/**
+ * A Pokémon that may be found in the wild holding an item. See:
+ * https://pokeapi.co/docs/v2#itemholderpokemon
+ *
+ * @param pokemon The Pokémon that holds this item.
+ * @param versionDetails The details for the version that this item is held in by the Pokémon.
+ */
 @Serializable
 @JsOnlyExport
 public data class ItemHolderPokemon(
@@ -32,10 +65,27 @@ public data class ItemHolderPokemon(
   val versionDetails: List<ItemHolderPokemonVersionDetail>,
 )
 
+/**
+ * The details of the version in which a Pokémon holds an item. See:
+ * https://pokeapi.co/docs/v2#itemholderpokemonversiondetail
+ *
+ * @param rarity How often this Pokémon holds this item in the wild.
+ * @param version The version in which the Pokémon holds the item.
+ */
 @Serializable
 @JsOnlyExport
 public data class ItemHolderPokemonVersionDetail(val rarity: Int, val version: NamedApiResource)
 
+/**
+ * Natural attributes of items, such as being countable or being usable in battle. See:
+ * https://pokeapi.co/docs/v2#item-attributes
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param items A list of items that have this attribute.
+ * @param names The name of this resource listed in different languages.
+ * @param descriptions The description of this resource listed in different languages.
+ */
 @Serializable
 @JsOnlyExport
 public data class ItemAttribute(
@@ -46,6 +96,16 @@ public data class ItemAttribute(
   val descriptions: List<Description>,
 )
 
+/**
+ * Item categories determine where items will be placed in the players bag. See:
+ * https://pokeapi.co/docs/v2#item-categories
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param items A list of items that are a part of this category.
+ * @param names The name of this resource listed in different languages.
+ * @param pocket The pocket items in this category are put into.
+ */
 @Serializable
 @JsOnlyExport
 public data class ItemCategory(
@@ -56,6 +116,15 @@ public data class ItemCategory(
   val pocket: NamedApiResource,
 )
 
+/**
+ * The various effects of the move "Fling" when used with different items. See:
+ * https://pokeapi.co/docs/v2#item-fling-effects
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param effectEntries The result of this fling effect listed in different languages.
+ * @param items A list of items that have this fling effect.
+ */
 @Serializable
 @JsOnlyExport
 public data class ItemFlingEffect(
@@ -65,6 +134,15 @@ public data class ItemFlingEffect(
   val items: List<NamedApiResource>,
 )
 
+/**
+ * Pockets within the players bag used for storing items by category. See:
+ * https://pokeapi.co/docs/v2#item-pockets
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param categories A list of item categories that are relevant to this item pocket.
+ * @param names The name of this resource listed in different languages.
+ */
 @Serializable
 @JsOnlyExport
 public data class ItemPocket(
