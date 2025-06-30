f11a25c988a3da26402e48d9a35f8667cbc4155e
Sargun Vohra <sargunv@users.noreply.github.com>
2025-06-30 00:51:44 -0400

    add kdoc for machines and locations

--- src/commonMain/kotlin/co/pokeapi/pokekotlin/model/locations.kt
+++ src/commonMain/kotlin/co/pokeapi/pokekotlin/model/locations.kt
@@ -3,6 +3,17 @@ package co.pokeapi.pokekotlin.model
 import co.pokeapi.pokekotlin.internal.JsOnlyExport
 import kotlinx.serialization.Serializable
 
+/**
+ * Locations that can be visited within the games. They make up sizable portions of regions, like
+ * cities or routes. See: https://pokeapi.co/docs/v2#locations
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param region The region this location can be found in.
+ * @param names The name of this resource listed in different languages.
+ * @param gameIndices A list of the indices of this location in different games.
+ * @param areas A list of areas that can be found in this location.
+ */
 @Serializable
 @JsOnlyExport
 public data class Location(
@@ -14,6 +25,20 @@ public data class Location(
   val areas: List<NamedApiResource>,
 )
 
+/**
+ * Location areas are sections of locations, such as floors in a building or cave. Each area has its
+ * own set of possible Pokémon encounters. See: https://pokeapi.co/docs/v2#location-areas
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param gameIndex The internal id of an API resource within game data.
+ * @param encounterMethodRates A list of methods in which Pokémon may be encountered in this area
+ *   and how likely the method is to occur depending on the version of the game.
+ * @param location The location this area can be found in.
+ * @param names The name of this resource listed in different languages.
+ * @param pokemonEncounters A list of Pokémon that can be encountered in this area along with
+ *   version specific details about the encounter.
+ */
 @Serializable
 @JsOnlyExport
 public data class LocationArea(
@@ -26,6 +51,13 @@ public data class LocationArea(
   val pokemonEncounters: List<PokemonEncounter>,
 )
 
+/**
+ * The encounter rate for a specific encounter method in a location area. See:
+ * https://pokeapi.co/docs/v2#encountermethodrate
+ *
+ * @param encounterMethod The method in which Pokémon may be encountered in an area.
+ * @param versionDetails The chance of the encounter to occur on a version of the game.
+ */
 @Serializable
 @JsOnlyExport
 public data class EncounterMethodRate(
@@ -33,10 +65,24 @@ public data class EncounterMethodRate(
   val versionDetails: List<EncounterMethodRateVersionDetail>,
 )
 
+/**
+ * The encounter rate for a specific encounter method in a location area and version. See:
+ * https://pokeapi.co/docs/v2#encountermethodrateversiondetail
+ *
+ * @param rate The chance of an encounter to occur.
+ * @param version The version of the game in which the encounter can occur with the given chance.
+ */
 @Serializable
 @JsOnlyExport
 public data class EncounterMethodRateVersionDetail(val rate: Int, val version: NamedApiResource)
 
+/**
+ * A Pokémon encounter in a specific location area. See: https://pokeapi.co/docs/v2#pokemonencounter
+ *
+ * @param pokemon The Pokémon being encountered.
+ * @param versionDetails A list of versions and encounters with Pokémon that might happen in the
+ *   referenced location area.
+ */
 @Serializable
 @JsOnlyExport
 public data class PokemonEncounter(
@@ -44,6 +90,15 @@ public data class PokemonEncounter(
   val versionDetails: List<VersionEncounterDetail>,
 )
 
+/**
+ * Areas used for grouping Pokémon encounters in Pal Park. They're like habitats that are specific
+ * to Pal Park. See: https://pokeapi.co/docs/v2#pal-park-areas
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param names The name of this resource listed in different languages.
+ * @param pokemonEncounters A list of Pokémon encountered in this pal park area along with details.
+ */
 @Serializable
 @JsOnlyExport
 public data class PalParkArea(
@@ -53,6 +108,14 @@ public data class PalParkArea(
   val pokemonEncounters: List<PalParkEncounterSpecies>,
 )
 
+/**
+ * A Pokémon species that can be encountered in a specific Pal Park area. See:
+ * https://pokeapi.co/docs/v2#palparkencounterspecies
+ *
+ * @param baseScore The base score given to the player when this Pokémon is caught.
+ * @param rate The base rate for encountering this Pokémon in this pal park area.
+ * @param pokemonSpecies The Pokémon species being encountered.
+ */
 @Serializable
 @JsOnlyExport
 public data class PalParkEncounterSpecies(
@@ -61,6 +124,19 @@ public data class PalParkEncounterSpecies(
   val pokemonSpecies: NamedApiResource,
 )
 
+/**
+ * A region is an organized area of the Pokémon world. Most often, the main difference between
+ * regions is the species of Pokémon that can be encountered within them. See:
+ * https://pokeapi.co/docs/v2#regions
+ *
+ * @param id The identifier for this resource.
+ * @param name The name for this resource.
+ * @param locations A list of locations that can be found in this region.
+ * @param mainGeneration The generation this region was introduced in.
+ * @param names The name of this resource listed in different languages.
+ * @param pokedexes A list of pokédexes that catalogue Pokémon in this region.
+ * @param versionGroups A list of version groups where this region can be visited.
+ */
 @Serializable
 @JsOnlyExport
 public data class Region(
--- src/commonMain/kotlin/co/pokeapi/pokekotlin/model/machines.kt
+++ src/commonMain/kotlin/co/pokeapi/pokekotlin/model/machines.kt
@@ -3,6 +3,16 @@ package co.pokeapi.pokekotlin.model
 import co.pokeapi.pokekotlin.internal.JsOnlyExport
 import kotlinx.serialization.Serializable
 
+/**
+ * Machines are the representation of items that teach moves to Pokémon. They vary from version to
+ * version, so it is not certain that one specific TM or TR works in every game. See:
+ * https://pokeapi.co/docs/v2#machines
+ *
+ * @param id The identifier for this resource.
+ * @param item The TM or HM item that corresponds to this machine.
+ * @param move The move that is taught by this machine.
+ * @param versionGroup The version group that this machine applies to.
+ */
 @Serializable
 @JsOnlyExport
 public data class Machine(
